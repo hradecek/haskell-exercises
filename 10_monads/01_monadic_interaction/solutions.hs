b85f04332392da3a549d27e24c5fcdaac6a0f6b0
Ivo Hradek <ihradek@redhat.com>
2017-08-25 19:51:19 +0200

    Added solutions

--- 10_monads/01_monadic_interaction/solutions.hs
+++ 10_monads/01_monadic_interaction/solutions.hs
@@ -6,11 +6,13 @@
 --------------------------------------------------------------------------------
 module Solutions_10_1 where
 
+import Data.Char (ord)
+
 -- | Ex. 10.1.1
 --
--- > (>>) = (->>)
-(>>-) :: Monad m => m a -> m b -> m b
-f >>- g = f >>= (\_ -> g)
+-- > (>>>) = (>>)
+(>>>) :: Monad m => m a -> m b -> m b
+f >>> g = f >>= (\_ -> g)
 
 -- | Ex. 10.1.2
 --
@@ -18,8 +20,39 @@ f >>- g = f >>= (\_ -> g)
 
 -- | Ex. 10.1.3
 -- | Ex. 10.1.4
--- | Ex. 10.1.5
--- | Ex. 10.1.6
--- | Ex. 10.1.7
--- | Ex. 10.1.8
+-- | Ex. 10.1.5a
+--
+-- Using operator (>>=), define operator (<>) such that:
+(<>) :: (a -> IO b) -> (b -> IO c) -> (a -> IO c)
+f <> g = \x -> f x >>= g
 
+-- | Ex. 10.1.5b
+--
+-- Using operator (<>) defined above, define operator (>>>=) such that:
+-- > (>>>=) = (>>=)
+--(>>>=) :: Monad m => m a -> (a -> m b) -> m b
+--f >>>= g = \_ -> f <> g
+
+-- | Ex. 10.1.6
+--
+-- @
+--   do { C; do { D; r}} ~>
+--   C >> do { D; r } ~>
+--   C >> D >> r ~>
+--   do { C; D; r }
+-- @
+
+-- | Ex. 10.1.7
+reveal :: IO ()
+reveal = do c  <- getChar
+            if c == 'q' then do
+                putChar '\n'
+                return ()
+            else do
+                putChar '\n'
+                putStrLn (show (ord c))
+                reveal
+
+-- | Ex. 10.1.8
+--
+-- See hangman.hs
